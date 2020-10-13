964f98cc51c7d1120e4e5b2be407a75334742128
Opilane TTHK <opilane@tthk.ee>
2020-10-13 09:55:19 +0300

    first commit

---
+++ SunMode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+
+namespace ChuckAPI
+{
+    class Program
+    {
+        private static string url = "https://api.sunrise-sunset.org/json?lat=59.4370&lng=24.7536&date=2020-10-13";
+        static void Main(string[] args)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+
+                SunMode sunMode = JsonConvert.DeserializeObject<SunMode>(response);
+                Console.WriteLine($"Sunrise: {sunMode.Results.Sunrise}.");
+                Console.WriteLine($"Sunset: {sunMode.Results.SunSet}. ");
+                Console.WriteLine($"Status: {sunMode.Status}. ");
+            }
+        }
+    }
+}
---
+++ SunMode/SunMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChuckAPI
+{
+    class SunMode
+    {
+        public Results Results { get; set; }
+        public string Status { get; set; }
+    }
+    public class Results
+    {
+        public string Sunrise { get; set; }
+        public string SunSet { get; set; }
+    }
+}
